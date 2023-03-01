1423d10a3af295eb19b6774287be2f6679980ce8
RosaD97 <r960595@gmail.com>
2023-03-01 17:04:20 +0100

    Parole Palindrome Fatto

--- Palidroma/main.js
+++ Palidroma/main.js
@@ -3,9 +3,23 @@
 const parolaInserita = prompt('Inserisci una parola');
 console.log(parolaInserita);
 
-let controllo = '';
+let parola1 = '';
+let parola2 = '';
 
-for(let i = parolaInserita.length - 1; i > parolaInserita; i--){
-    controllo = parolaInserita[i];
-    console.log(controllo);
+for(let i = 0; i < parolaInserita.length; i++){
+    parola1 = parolaInserita[i];
 }
+
+for(let i = parolaInserita.length - 1; i >= 0; i--){
+    parola2 = parolaInserita[i];
+}
+
+function palindroma (text1, text2){
+    if (parola1 === parola2){
+        console.log('palindroma');
+    }
+    return text1 === text2;
+}
+
+const palindromaResult = palindroma(parola1, parola2);
+console.log(palindromaResult)
