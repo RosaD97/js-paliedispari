c528d5ac370d06d92190732f90ef86afd731a5cb
RosaD97 <r960595@gmail.com>
2023-03-01 15:51:35 +0100

    Gioco pari e dispari fatto

--- Palidroma/main.js
+++ Palidroma/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const parolaInserita = prompt('Inserisci una parola');
+console.log(parolaInserita);
+
+let controllo = '';
+
+for(let i = parolaInserita.length - 1; i > parolaInserita; i--){
+    controllo = parolaInserita[i];
+    console.log(controllo);
+}
--- Pari e Dispari/main.js
+++ Pari e Dispari/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const choose = prompt('Scrivi: pari o dispari');
+const numero = Number(prompt('Scrivi un numero tra 1 e 5'));
+const computer = numeriACaso(1, 6);
+
+function numeriACaso(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+function pariDispari (num1, num2){
+    console.log(num1)
+    console.log(num2)
+    const sum = num1 + num2;
+    return sum;
+}
+
+const risultato = pariDispari(numero, computer);
+console.log(risultato)
+
+if (risultato % 2 === 0 && choose === 'pari' || risultato % 2 !== 0 && choose === 'dispari') {
+    alert('hai vinto')
+}else {
+    alert('hai perso')
+}
